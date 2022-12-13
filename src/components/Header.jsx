066b9d7609f88c26fcb52eb5e63315cb7b075930
pavel99pop <pavel99pop@gmail.com>
2022-12-13 20:30:06 +0600

    Styled Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import WorldIcon from "../images/world.svg";
 export default function Header() {
   return (
     <div className="header">
-      <img src={WorldIcon} alt="World Icon" />
+      <img className="header-icon" src={WorldIcon} alt="World Icon" />
       <h1>my travel journal</h1>
     </div>
   );
