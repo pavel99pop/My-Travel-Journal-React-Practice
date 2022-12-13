14138607b9a912bca71389281d79864d83473f9c
pavel99pop <pavel99pop@gmail.com>
2022-12-13 20:23:35 +0600

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const clearCacheData = () => {
-    caches.keys().then((names) => {
-      names.forEach((name) => {
-        caches.delete(name);
-      });
-    });
-    alert("Complete Cache Cleared");
-  };
-
   return (
-    <div style={{ height: 500, width: "80%" }}>
-      <h4>How to clear complete cache data in ReactJS?</h4>
-      <button onClick={() => clearCacheData()}>Clear Cache Data</button>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import WorldIcon from "../images/world.svg";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src={WorldIcon} alt="World Icon" />
+      <h1>my travel journal</h1>
+    </div>
+  );
+}
