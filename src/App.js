473ef7bd0b712c5d6406f5454f3fa5b81659ebeb
pavel99pop <pavel99pop@gmail.com>
2022-12-13 21:05:33 +0600

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="Snapshot" />
+      </div>
+      <div>
+        <div>
+          <img src="" alt="Location Pin" />
+          <p>Country</p>
+          <a href="">View on Google Maps</a>
+        </div>
+        <div>
+          <h2>Title</h2>
+          <p>Timeline</p>
+          <p>Description</p>
+        </div>
+      </div>
+    </div>
+  );
+}
